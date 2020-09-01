eac96844f304edb9b63695252e775a364edb1098
aishugundmi <aishwarya.udupi1@gmail.com>
2020-09-01 11:17:40 +0530

    LED_Roll with R_P_Y commit

--- main.c
+++ main.c
@@ -36,9 +36,9 @@
 
 #define MPU6050_RA_WHO_AM_I 0x75
 
-#define ACCEL_RANGE		((float) 16384)
-#define GYRO_RANGE		((float) 131)
 #define PI  3.141592
+
+
 /**............................................................................................................*/
 // MPU6050 Slave Device Address
 const uint8_t MPU6050_Address = (0x68 << 1);
@@ -61,6 +61,8 @@ const uint8_t MPU6050_SIGNAL_PATH_RESET  = 0x68;
 uint8_t ser[14];
 char print_buf1[100];
 volatile uint8_t int_rupt = 0;
+
+
 /**............................................................................................................*/
 
 typedef struct
@@ -94,9 +96,15 @@ ISR(INT0_vect)
 	int_rupt = 1;
 }
 
+void timer0pwm_init()
+{
+    TCCR0A |= (1<<COM0A1) | (1<<COM0B1) | (1<<WGM01) | (1<<WGM00);
+    TCCR0B |= (1<<CS00);  //pre scaling 1
+    sei();
+}
 
 
-//configure MPU6050
+// configure MPU6050
 void MPU6050_Init()
 {
     i2c_write_byte(MPU6050_Address, MPU6050_SMPLRT_DIV, 0x4F);  //(8000/100)-1=79
@@ -139,7 +147,10 @@ int main(void)
 {
 
     DDRD |= (0<<PD2);    //PORTD as input INT0
-    DDRD |= (1<<DDD3);
+    DDRD |= (1<<PD3);    //ouput LED
+
+    DDRD |= (1<<PD5);
+    DDRD |= (1<<PD6);
 
     EICRA |= (1 << ISC00) | (1 << ISC01);    // set INT0 to trigger on the rising edge
     EIMSK |= (1 << INT0);                    // Turns on INT0
@@ -148,6 +159,7 @@ int main(void)
     i2c_init();
     USART_init();
     MPU6050_Init();
+    timer0pwm_init();
 
     uint8_t res;
     i2c_read_byte(MPU6050_Address, MPU6050_RA_WHO_AM_I, &res);
@@ -162,24 +174,24 @@ int main(void)
     uint16_t z=0;
     int32_t add_Gx=0, add_Gy=0, add_Gz=0;
     int16_t Gx_offset = 0, Gy_offset = 0, Gz_offset = 0;
-    int16_t angle_Gx = 0, angle_Gy = 0, angle_Gz = 0;
-    int32_t agl_Gx = 0, agl_Gy = 0, agl_Gz = 0;
-    int16_t roll = 0, pitch = 0;
+    float angle_Gx = 0, angle_Gy = 0, angle_Gz = 0;
+    float G_pitch = 0, G_roll = 0, G_yaw = 0;
+    int16_t A_roll = 0, A_pitch = 0;
 
-    while(z < 1000)
+    while(z < 500)
     {
         read_mpu(&tmp);
         add_Gx += tmp.Gx;
         add_Gy += tmp.Gy;
         add_Gz += tmp.Gz;
 
-        _delay_ms(4);
+        _delay_ms(10);
         z++;
     }
-    Gx_offset =  (add_Gx/1000);
-    Gy_offset =  (add_Gy/1000);
-    Gz_offset =  (add_Gz/1000);
-
+    Gx_offset =  (add_Gx/500);
+    Gy_offset =  (add_Gy/500);
+    Gz_offset =  (add_Gz/500);
+	int_rupt = 1;
     while(1)
     {
         if(int_rupt == 1)
@@ -189,25 +201,50 @@ int main(void)
                 read_mpu(&tmp);
 
          //       w_Gx = (tmp.Gx - Gx_offset) * (2000.0/32768.0);
-         //       angle_Gx += (w_Gx * 0.01 *1000);
+         //       angle_Gx += (w_Gx * 0.01);
 
-                angle_Gx = (((int32_t)(tmp.Gx - Gx_offset) * 20000UL)/32768UL);
-                angle_Gy = (((int32_t)(tmp.Gy - Gy_offset) * 20000UL)/32768UL);
-                angle_Gz = (((int32_t)(tmp.Gz - Gz_offset) * 20000UL)/32768UL);
+                angle_Gx =  ((float)(tmp.Gx - Gx_offset) * 20.0/32768.0);
+                angle_Gy =  ((float)(tmp.Gy - Gy_offset) * 20.0/32768.0);
+                angle_Gz =  ((float)(tmp.Gz - Gz_offset) * 20.0/32768.0);
 
-                agl_Gx -= angle_Gx;
-                agl_Gy -= angle_Gy;
-                agl_Gz += angle_Gz;
+                G_roll += angle_Gx;
+                G_pitch  -= angle_Gy;
+                G_yaw += angle_Gz;
 
-                // Calculating Roll and Pitch from the accelerometer data
-                roll = (atan2((double)(-tmp.Ay), (double)tmp.Az) * 180.0)/PI;
-                pitch = (atan2((double)tmp.Ax, sqrt((double) tmp.Ay*tmp.Ay +  (double) tmp.Az*tmp.Az)) * 180.0)/PI;
+                ///  Calculating Roll and Pitch from the accelerometer data
+                A_roll = (atan2((float)(tmp.Ay), (float)tmp.Az) * 180.0)/PI;
+                A_pitch = (atan2((float)tmp.Ax, sqrt((float) tmp.Ay*tmp.Ay +  (float) tmp.Az*tmp.Az)) * 180.0)/PI;
+
+
+				///  COMPLEMENTARY FILTERING of G_roll and G_pitch
+				G_roll = (float)G_roll*.98 + (float)A_roll*.02;
+				G_pitch = (float)G_pitch*.98 + (float)A_pitch*.02;
+
+
+                int8_t a = (int8_t)G_roll;
+
+                if((0 < a) && (a < 91) )
+                {
+                    OCR0A = (uint8_t)((a * 255)/90);
+                    OCR0B = 0;
+                }
+                else if((-91 < a) && (a < 0))
+                {
+                    OCR0B = (uint8_t)(-1 * (a * 255)/90);
+                    OCR0A = 0;
+                }
+                else
+                {
+                    OCR0A = 0;
+                    OCR0B = 0;
+                }
 
                 var++;
 
+
             if(var == 10)
             {
-               sprintf(print_buf1, "Ar= %d     Ap= %d     Gr= %ld       Gp= %ld      Gy= %ld  deg\n", roll, pitch, (agl_Gx/1000),(agl_Gy/1000), (agl_Gz/1000));
+               sprintf(print_buf1, "Ar= %d\tAp= %d\tGr= %d\tGp= %d\tGy= %d\n", A_roll, A_pitch, (int)(G_roll), (int)(G_pitch), (int)(G_yaw));
                USART_putstring(print_buf1);
                var = 0;
             }
